c6d9a8de4cfedc39a5dad0a601e29d127dda59d1
Samir Gadkari <samir.gadkari@gmail.com>
2019-02-11 19:10:49 -0800

    Added pandas and numpy to setup.py

--- lambdata_samirgadkari/__init__.py
+++ lambdata_samirgadkari/__init__.py
@@ -1,1 +1,6 @@
+import pandas as pd
+import numpy as np
+
 VERSION = 0
+
+ONES = np.ones(100)
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+Package setup installation and metadata for lambdata
+"""
+
+import setuptools
+
+REQUIRED = [
+    'numpy', # You can put conditions here like 'numpy >= 2.3.0'
+    'pandas'
+]
+
+with open('README.md', 'r') as fh:
+    LONG_DESCRIPTION = fh.read()
+
+# This is all the stuff we're telling pypi
+setuptools.setup(
+    name='lambdata-samirgadkari',
+    version='0.0.2',  # We have to edit this if we changed code and are now
+                      # pushing it.
+    author='samirgadkari',
+    description='A collection of data science helper functions.',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/samirgadkari/lambdata',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.5',
+    install_requires=REQUIRED,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent",
+    ]
+)
